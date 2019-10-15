5e823f14473c670206124c243d5fbe37be619d6d
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2019-10-15 23:54:02 +0200

    Api Stubs for Processing the forms

---
+++ api/v3/Drupalcms/Postlookups.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Klaas Eikelboom  <klaas@kainuk.it>
+ * @date 15-Oct-2019
+ * @license AGPL-3.0
+ */
+use CRM_CMS_ExtensionUtil as E;
+
+function _civicrm_api3_Drupalcms_postlookups_spec(&$spec) {
+}
+
+
+function civicrm_api3_Drupalcms_postlookups($params) {
+    $returnValues = [];
+    return civicrm_api3_create_success($returnValues, $params, 'Drupalcms', 'PostLookups');
+}
