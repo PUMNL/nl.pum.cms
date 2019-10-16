5a9c4a47962f783c785fe505503e474c896b8c68
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2019-10-16 23:48:02 +0200

    Added email to the configuration screen

--- CRM/CMS/Form/Preferences.php
+++ CRM/CMS/Form/Preferences.php
@@ -8,26 +8,35 @@ use CRM_CMS_ExtensionUtil as E;
  * @see https://docs.civicrm.org/dev/en/latest/framework/quickform/
  */
 class CRM_CMS_Form_Preferences extends CRM_Admin_Form_Preferences {
+
   public function preProcess() {
     CRM_Utils_System::setTitle(ts('CMS Drupal Services - connectivity params'));
     $this->_varNames = [
       'Drupal CMS Api' => [
-        'drupal_cms_url' =>  [
+        'drupal_cms_url' => [
           'html_type' => 'text',
           'title' => ts('Remote REST Api URL'),
-          'size'  => 64,
+          'size' => 64,
           'weight' => 3,
           'description' => ts('Example https://example.com/sites/all/modules/civicrm/extern/rest.php'),
         ],
-        'drupal_cms_authtoken' =>  [
+        'drupal_cms_authtoken' => [
           'html_type' => 'text',
           'title' => ts('Authorization token for the Remote call'),
-          'size'  => 64,
+          'size' => 64,
           'weight' => 4,
           'description' => ts('jdwuubbdyebeu'),
         ],
+        'drupal_cms_email' => [
+          'html_type' => 'text',
+          'title' => ts('Email were an error in case of failure goes to '),
+          'size' => 64,
+          'weight' => 4,
+          'description' => ts('helpdesk'),
         ],
-      ];
+      ],
+    ];
     parent::preProcess();
   }
+
 }
