d0b631463edf63a3c17fec7926b793658d1b5cec
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2019-12-18 10:29:14 +0100

    Getsubmission did not have a email warning

--- api/v3/Drupalcms/Getsubmissions.php
+++ api/v3/Drupalcms/Getsubmissions.php
@@ -10,8 +10,18 @@ function _civicrm_api3_Drupalcms_getsubmissions_spec(&$spec) {
 
 
 function civicrm_api3_Drupalcms_getsubmissions($params) {
-    $returnValues = [];
-    $submissionProcessor = new CRM_CMS_SubmissionProcessor();
-    $submissionProcessor->process();
-    return civicrm_api3_create_success($returnValues, $params, 'Drupalcms', 'Getsubmissions');
+    try {
+        $returnValues = [];
+        $submissionProcessor = new CRM_CMS_SubmissionProcessor();
+        $submissionProcessor->process();
+        return civicrm_api3_create_success($returnValues, $params, 'Drupalcms', 'Getsubmissions');
+    } catch (Exception $ex) {
+        $mailError = new CRM_CMS_MailError();
+        $mailError->setError($ex);
+        civicrm_api3('Email', 'Send', [
+            'contact_id' => CRM_Core_BAO_Setting::getItem('Drupal CMS Api', 'drupal_cms_contact_id'),
+            'template_id' => CRM_Core_BAO_Setting::getItem('Drupal CMS Api', 'drupal_cms_template_id'),
+        ]);
+        throw new Exception($ex);
+    }
 }
