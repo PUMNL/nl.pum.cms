43b25f31668b1bdcfe6509be3447a4f2b67e355b
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2019-12-18 10:32:04 +0100

    Added email warning to remove or records.

--- api/v3/Drupalcms/Remove.php
+++ api/v3/Drupalcms/Remove.php
@@ -11,18 +11,28 @@ function _civicrm_api3_Drupalcms_remove_spec(&$spec) {
 
 function civicrm_api3_Drupalcms_remove($params)
 {
-    $returnValues = [];
-    $rest = new CRM_CMS_Rest();
-    $count = 0;
+    try {
+        $returnValues = [];
+        $rest = new CRM_CMS_Rest();
+        $count = 0;
 
-    $dao = CRM_Core_DAO::executeQuery('select id, entity, submission_id from pum_cms_submission');
-    while($dao->fetch()){
-        $rest->delete($dao->entity,$dao->submission_id);
-        CRM_Core_DAO::executeQuery('delete from pum_cms_submission where id=%1',[
-            1 => [$dao->id,'Integer'],
+        $dao = CRM_Core_DAO::executeQuery('select id, entity, submission_id from pum_cms_submission');
+        while ($dao->fetch()) {
+            $rest->delete($dao->entity, $dao->submission_id);
+            CRM_Core_DAO::executeQuery('delete from pum_cms_submission where id=%1', [
+                1 => [$dao->id, 'Integer'],
+            ]);
+            $count++;
+        }
+        $return['count'] = $count;
+        return civicrm_api3_create_success($returnValues, $params, 'Drupalcms', 'Remove');
+    } catch (Exception $ex) {
+        $mailError = new CRM_CMS_MailError();
+        $mailError->setError($ex);
+        civicrm_api3('Email', 'Send', [
+            'contact_id' => CRM_Core_BAO_Setting::getItem('Drupal CMS Api', 'drupal_cms_contact_id'),
+            'template_id' => CRM_Core_BAO_Setting::getItem('Drupal CMS Api', 'drupal_cms_template_id'),
         ]);
-        $count++;
+        throw new Exception($ex);
     }
-    $return['count']= $count;
-    return civicrm_api3_create_success($returnValues, $params, 'Drupalcms', 'PostLookups');
 }
